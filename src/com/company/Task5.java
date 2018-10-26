b23419844775a5cdd569036cd0ad1455bc88a41f
Slava <44290297+Slava-Legedin@users.noreply.github.com>
2018-10-26 12:04:11 +0300

    last commit 26-10-18!

--- src/com/company/Task5.java
+++ src/com/company/Task5.java
@@ -1,27 +1,42 @@
-package com.company;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class Task5 {
 
-    public static void Tasks() {
-        Scanner read = new Scanner(System.in);
-        String string;
 
-        System.out.println("Please enter something: ");
-        List<String> list;
-        list = new ArrayList<>();
-        while (read.toString() != ("1")){
+    //public static List<String> prio = new ArrayList<String>();
+    public static List<Integer> prio = new ArrayList<Integer>();
+    public static List<String> compl = Arrays.asList("COMPLEX", "MIDDLE", "EASY");
+    //public static List<String> pr_list = Arrays.asList("COMPLEX", "MIDDLE", "EASY");
+    public static void Tasks(){
+        Scanner scanner = new Scanner(System.in);
+        //System.out::println(String.valueOf(ArrayList[]));
 
-            System.out.println("Please enter something: ");
-                read.toString();
-                list.add(read.nextLine());
-                System.out.println(list);
+        while (true) {
 
+            System.out.printf("Please enter Priority from 1 to 3: ");
+
+            Integer pr = scanner.nextInt();
+            if (pr >= 1 && pr <= 3) {
+                prio.add(pr);
+            }
+            else {
+                System.out.println("Please enter correct digits!!!");
+            }
+  /*
+            System.out.printf("Please enter Complexity: ");
+            prio.add(scanner.next());
+*/
+            System.out.printf("Please enter ###");
+            if (scanner.next().equals("test")) {
+                break;
+            }
 
         }
 
+
+        prio.forEach(System.out::println);
     }
+
+
 }
